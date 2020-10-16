2ebeb3cc5693d832a28e1a0d6416c8d1c06c22c5
Hayden Lewis <whlewis@g.clemson.edu>
2020-10-16 11:31:14 -0500

    Update

---
+++ gVRage/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void resetTutorial()
+    {
+
+    }
+
+    public void nextStep()
+    {
+
+    }
+
+    public void previousStep()
+    {
+
+    }
+}
