6e83ca8dc63784b88e628633f928ae6827b6a9e9
ldnljcksn <ldnljcksn@gmail.com>
2020-11-11 01:15:01 -0500

    kinda pick up

--- gVRage/Assets/Scripts/PickUp.cs
+++ gVRage/Assets/Scripts/PickUp.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Transform theDest;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void OnMouseDown()
+  {
+    GetComponent<Rigidbody>().useGravity = false;
+    this.transform.position = theDest.position;
+    this.transform.parent = GameObject.Find("Destination").transform;
+  }
+
+  void OnMouseUp()
+  {
+    this.transform.parent = null;
+    GetComponent<Rigidbody>().useGravity = true;
+  }
 }
