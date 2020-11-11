76b08dfdad7b3195038fce2ad9978e48ee94c1c8
ldnljcksn <ldnljcksn@gmail.com>
2020-11-11 00:33:00 -0500

    added reticle

--- gVRage/Assets/Scripts/SC_FPSController.cs
+++ gVRage/Assets/Scripts/SC_FPSController.cs
@@ -32,6 +32,11 @@ public class SC_FPSController : MonoBehaviour
 
     void Update()
     {
+        if (CorkBoardMenu.GameIsPaused)
+        {
+          return;
+        }
+
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
